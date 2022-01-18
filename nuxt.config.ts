b3cbae48b7b08a1f4e4ca47566e02167181febb1
Jonas <jonasteichrueb@gmail.com>
2022-01-18 22:20:21 +0100

    Adding Google Map and making Modal work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
-export default defineNuxtConfig({
-
+export default defineNuxtConfig({ 
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: [
+    "~/assets/css/tailwind.css",
+    '@fortawesome/fontawesome-free/css/all.min.css'
+  ]
 })
