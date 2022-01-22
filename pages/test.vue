2e7121ecd87498832a5bcef017d578e668a0df3a
Richard <richardwinskontakt@gmail.com>
2022-01-22 16:56:15 +0100

    fix data fetching on test page

--- pages/test.vue
+++ pages/test.vue
@@ -1,9 +1,16 @@
 <template>
+  <p>Pending: {{ pending }}</p>
+  <p>Error: {{ error }}</p>
   <h1>
     Test: {{ data }}
   </h1>
 </template>
 
-<script setup>
-const data = await $fetch('api/now')
+<script>
+export default defineComponent({
+  async setup() {
+    const test = await useFetch('/api/now', { server: false })
+    return { data: test.data, pending: test.pending, error: test.error }
+  }
+})
 </script>
